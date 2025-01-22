4d4ad0ac02f62ffcfa53989fe7c1e7a20c9ae99c
nicoargiolas <argiolas.nicola00@gmail.com>
2025-01-22 15:21:02 +0100

    Aggiunta bottone reset

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 
 // Collegamento a elementi del DOM
 const emailListHTML = document.getElementById('email-list');
+const resetButtonHTML = document.getElementById('reset-btn');
 
-const emailList = [];
-
-let emailNumber = 10;
-for (let i = 0; i < emailNumber; i++) {
+function getNEmails(emailN) {
+    const emailList = [];
+for (let i = 0; i < emailN; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(response => {
         let mail = response.data.response;
         // emailListHTML.innerHTML += `<li> ${mail} </li>`
         emailList.push(mail);
-        if (emailList.length === emailNumber) {
+        if (emailList.length === emailN) {
             console.log(emailList);
-            for (let i = 0; i < emailNumber; i++) {
+            for (let i = 0; i < emailN; i++) {
                 emailListHTML.innerHTML += `<li> ${emailList[i]} </li>`
             }
         }
@@ -29,8 +29,18 @@ for (let i = 0; i < emailNumber; i++) {
         console.error(error)
     });
 }
+}
 
-// Ho usato questa soluzione per far si che la lista delle email comparisse contemporaneamente in pagina e non una dopo l'altra come nel precedente commit, so di aver appesantito il codice ma ho cercato di ragionare per ottenere una soluzione visiva più pulita
+let emailNumber = 10;
+getNEmails(emailNumber);
+
+resetButtonHTML.addEventListener("click", () => {
+    emailListHTML.innerHTML = "";
+    getNEmails(emailNumber);
+});
+
+
+// Ho usato questa soluzione per far si che la lista delle email comparisse contemporaneamente in pagina e non una dopo l'altra come nel primo commit, so di aver appesantito il codice ma ho cercato di ragionare per ottenere una soluzione visiva più pulita
 
 
 
