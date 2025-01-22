f7b38ff3f25cdca3b4c0dbb2a82fbf2bcf7d1b11
nicoargiolas <argiolas.nicola00@gmail.com>
2025-01-22 15:10:12 +0100

    Modifica per soluzione visiva più pulita

--- script.js
+++ script.js
@@ -9,15 +9,28 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 // Collegamento a elementi del DOM
 const emailListHTML = document.getElementById('email-list');
 
+const emailList = [];
 
 let emailNumber = 10;
 for (let i = 0; i < emailNumber; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(response => {
         let mail = response.data.response;
-        emailListHTML.innerHTML += `<li> ${mail} </li>`
+        // emailListHTML.innerHTML += `<li> ${mail} </li>`
+        emailList.push(mail);
+        if (emailList.length === emailNumber) {
+            console.log(emailList);
+            for (let i = 0; i < emailNumber; i++) {
+                emailListHTML.innerHTML += `<li> ${emailList[i]} </li>`
+            }
+        }
     })
     .catch(error => {
         console.error(error)
     });
 }
+
+// Ho usato questa soluzione per far si che la lista delle email comparisse contemporaneamente in pagina e non una dopo l'altra come nel precedente commit, so di aver appesantito il codice ma ho cercato di ragionare per ottenere una soluzione visiva più pulita
+
+
+
